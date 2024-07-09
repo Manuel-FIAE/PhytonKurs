2cb3cdbc007b26b41ce56403fca01e5781d1a785
Manuel-FIAE <manuel-perrotta@gmx.de>
2024-07-09 11:21:42 +0200

    neue aufgabe

---
+++ src/Zusatzaufgabe_zusammenfügen_von_Listen.py
@@ -0,0 +1,11 @@
+capitalsOfAfrica = ["Harare", "Pretoria", "Nairobi", "Kairo"]
+capitalsOfEurope = ["Oslo", "Bern", "Paris", "Berlin"]
+
+# Zusammenführen der Listen
+capitalsOfTheWorld = capitalsOfAfrica + capitalsOfEurope
+
+# Sortieren der kombinierten Liste
+capitalsOfTheWorld.sort()
+
+# Ausgabe der sortierten Liste
+print(capitalsOfTheWorld)
